fc3f28924f8247ecb4c06b6f2240010bff089f3b
Sergio Sánchez Vallés <serindio@gmail.com>
2019-04-16 23:07:43 +0200

    Update Show return information

--- src/scrapers/ShowScraper.py
+++ src/scrapers/ShowScraper.py
@@ -11,7 +11,28 @@ class ShowScraper(AbstractScraper):
         if file_extension in self.video_extensions:
             info = guessit(filename)
 
-            show = self.tmdb.search_show(info['title'])
-            r = self.tmdb.get_episode(tv_id=show['id'], season_number=info['season'], episode_number=info['episode'])
+            show = self.tmdb.search_show(info['title'])  # TODO Get all information from this request
+            episode = self.tmdb.get_episode(tv_id=show['id'], season_number=info['season'], episode_number=info['episode'])
+            result = {
+                'show': {
+                    'original_name': show['original_name'],
+                    'id': show['id'],
+                    'vote_average': show['vote_average'],
+                    'poster_path': show['poster_path'],
+                    'genre_ids': show['genre_ids'],
+                    'backdrop_path': show['backdrop_path'],
+                    'overview': show['overview'],
+                    'origin_country': show['origin_country']
+                },
 
-            return r
+                'episode': {
+                    'episode_number': episode['episode_number'],
+                    'name': episode['name'],
+                    'overview': episode['overview'],
+                    'season_number': episode['season_number'],
+                    'still_path': episode['still_path'],
+                    'vote_average': episode['vote_average']
+                }
+            }
+
+            return result
